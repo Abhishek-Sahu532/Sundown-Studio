cb5e856031b7c56ad159d7ba50acaafb1bf86bfe
Abhishek-Sahu532 <asahu532@gmail.com>
2024-11-20 18:53:21 +0530

    working on the responsive part

--- script.js
+++ script.js
@@ -38,15 +38,57 @@ function swiperAnimation() {
 
 swiperAnimation();
 
+
+// mobile menu started
 var menu = document.querySelector("nav h3");
 
 var full = document.querySelector("#full-scr");
 var closeBtn = document.querySelector(".close-btn");
 
+
+var navimg = document.querySelector('nav img')
+
 menu.addEventListener("click", () => {
   full.style.top = 0;
+  navimg.style.opactiy = 0
 });
 
 closeBtn.addEventListener("click", () => {
   full.style.top = "-100%";
 });
+
+// mobile menu ended
+
+const design = document.querySelector('#design')
+const project = document.querySelector('#project')
+const execution = document.querySelector('#execution')
+const imagePart = document.querySelector("#image-part")
+
+design.addEventListener('click', ()=>{
+  console.log('clicked design')
+  imagePart.src = "/assets/pageDesign/design1.webp"
+  design.classList.add('active') 
+  project.classList.remove('active') 
+  execution.classList.remove('active') 
+})
+
+project.addEventListener('click', ()=>{
+  console.log('clicked project')
+  imagePart.src = "/assets/pageDesign/project1.webp"
+  project.classList.add('active') 
+  execution.classList.remove('active')
+  design.classList.remove('active')
+})
+
+execution.addEventListener('click', ()=>{
+  console.log('clicked execution')
+  imagePart.src = "/assets/pageDesign/execution1.webp"
+  execution.classList.add('active') 
+  project.classList.remove('active') 
+  design.classList.remove('active') 
+})
+
+imagePart.addEventListener('click', ()=>{
+  console.log('clicked imagePart')
+
+})
